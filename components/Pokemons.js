71d5955630e9e6640b78e2d1b060975135c5bb46
Lenovo <p.castrec@gmail.com>
2022-11-03 11:09:05 +0100

    observer

--- components/Pokemons.js
+++ components/Pokemons.js
@@ -6,34 +6,46 @@ import '../public/style.css'
 
 export const Pokemons = () => {
 
+    const loader = React.useRef(null)
+
     const [input, setInput] = React.useState('')
     const [loading, setLoading] = React.useState(false)
     const [pokemons, setPokemons] = React.useState([])
+    const [settings, setSettings] = React.useState({ limit: 20, offset: 0 })
+
+    React.useEffect(() => {
+        const options = { root: null, rootMargin: "20px", threshold: 0 }
+        const observer = new IntersectionObserver(handleObserver, options)
+        if (loader.current) observer.observe(loader.current)
+    }, [handleObserver])
 
     React.useEffect(() => {
         setLoading(true)
-        fetch('https://pokeapi.co/api/v2/pokemon')
+        fetch(`https://pokeapi.co/api/v2/pokemon?limit=${settings.limit}&offset=${settings.offset}`)
             .then(res => res.json())
-            .then(data => setPokemons(data.results))
+            .then(data => setPokemons(prev => [...prev, ...data.results]))
             .catch(err => console.log(err))
             .finally(() => setLoading(false))
-    }, [])
+    }, [settings])
+
+    const handleObserver = React.useCallback(entries => {
+        if (entries[0].isIntersecting) {
+            console.log('loading')
+            setSettings(prev => ({ limit: 20, offset: prev.offset + 20 }))
+        }
+    }, [loading])
 
     return (
         <>
             <SearchBar input={input} setInput={setInput} />
-            {
-                loading ?
-                    <h1>Not Found</h1>
-                    :
-                    <div className="container list">
-                        {
-                            pokemons
-                            .filter((p, i) => p.name.includes(input.toLowerCase()))
-                            .map((p, i) => <Pokemon key={i} data={p} />)
-                        }
-                    </div>
-            }
+            <div className="container list" > {/*onScroll={handleScroll}*/}
+                {
+                    pokemons
+                        .filter((p, i) => p.name.includes(input.toLowerCase()))
+                        .map((p, i) => <Pokemon key={i} data={p} />)
+                }
+            </div>
+            {!loading && <div ref={loader}></div>}
         </>
     )
 }
